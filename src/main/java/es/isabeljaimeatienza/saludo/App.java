87e31a1daa1ab83e04266bcd84c4d8778756bffc
cadit <cadit@DESKTOP-PCOVD1I>
2020-04-24 19:16:06 +0200

    Aprobaremos

--- src/main/java/es/isabeljaimeatienza/saludo/App.java
+++ src/main/java/es/isabeljaimeatienza/saludo/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
 
-        var label = new Label("Isa");
+        var label = new Label("Aprobaremos");
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
         stage.show();
